c968bfe97466c84a9d7d6fd35fb229f3466958be
Choi Seunghun <0015chseang@gmail.com>
2024-06-06 11:57:45 +0900

    fix: 논문 재현 모델 수정

--- model/model.py
+++ model/model.py
@@ -4,25 +4,7 @@ import torch.nn.functional as F
 from base import BaseModel
 
 
-class ECGModel2(BaseModel):
-    def __init__(self, num_classes=5):
-        super().__init__()
-        self.conv1 = nn.Conv1d(1, 16, kernel_size=5, stride=1)
-        self.conv2 = nn.Conv1d(16, 32, kernel_size=5, stride=1)
-
-        self.fc1 = nn.Linear(1376, 120)
-        self.fc2 = nn.Linear(120, num_classes)
-
-    def forward(self, x):
-        x = F.relu(F.max_pool1d(self.conv1(x), 2))
-        x = F.relu(F.max_pool1d(self.conv2(x), 2))
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
-
-
+# 논문 재현
 class ECGModel(BaseModel):
     def __init__(self, num_classes=5):
         super().__init__()
@@ -62,3 +44,23 @@ class ECGModel(BaseModel):
         x = self.softmax(x)
 
         return x
+
+
+# conv 2개 붙여서 만든 모델
+class ECGModel2(BaseModel):
+    def __init__(self, num_classes=5):
+        super().__init__()
+        self.conv1 = nn.Conv1d(1, 16, kernel_size=5, stride=1)
+        self.conv2 = nn.Conv1d(16, 32, kernel_size=5, stride=1)
+
+        self.fc1 = nn.Linear(1376, 120)
+        self.fc2 = nn.Linear(120, num_classes)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool1d(self.conv1(x), 2))
+        x = F.relu(F.max_pool1d(self.conv2(x), 2))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
